674f65ca2979a9d3d27515946d60bc84e42b23fd
Nouira Baligh <nouirabaligh009@gmail.com>
2024-11-06 15:15:17 +0100

    worked in detailled product page

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,50 +1,53 @@
-import Cart from "./pages/Cart"
-import Home from "./pages/Home"
-import {  RouterProvider,createBrowserRouter,Outlet } from "react-router-dom"
-import MyAccount from "./pages/MyAccount"
-import Footer from "./components/Footer.jsx"
-import Navbar from "./components/Navbar.jsx"
-import Announcement from "./components/Announcement.jsx"
+import Cart from "./pages/Cart";
+import Home from "./pages/Home";
+import { RouterProvider, createBrowserRouter, Outlet } from "react-router-dom";
+import MyAccount from "./pages/MyAccount";
+import Footer from "./components/Footer.jsx";
+import Navbar from "./components/navbar.jsx";
+import Announcement from "./components/Announcement.jsx";
+import Product from "./pages/Product.jsx";
 function App() {
-const Layout=()=>{
-  return <>
-  <Announcement/>
-  <Navbar />
-  <Outlet/>
-  <Footer/>
-  </>
-}
+  const Layout = () => {
+    return (
+      <>
+        <Announcement />
+        <Navbar />
+        <Outlet />
+        <Footer />
+      </>
+    );
+  };
 
-  
-const router= createBrowserRouter([
-  {
-    path:"/",
-    element:<Layout/>,
-    children:[
-      {
-        path:"/",
-        element:<Home/>
-    
-      },, {
-        path:"/cart",
-        element:<Cart/>
-    
-      },
-      {
-        path:"/myaccount",
-        element:<MyAccount/>
-    
-      }
-    ]
-
-  },
-
-])
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout />,
+      children: [
+        {
+          path: "/",
+          element: <Home />,
+        },
+        ,
+        {
+          path: "/cart",
+          element: <Cart />,
+        },
+        {
+          path: "/myaccount",
+          element: <MyAccount />,
+        },
+        {
+          path: "/product/:productId",
+          element: <Product />,
+        },
+      ],
+    },
+  ]);
   return (
     <>
-<RouterProvider router={router} />    
-</>
-  )
+      <RouterProvider router={router} />
+    </>
+  );
 }
 
-export default App
+export default App;
